881b8ce292b510cdcb3a593d5700bc98af877d07
sheqiao <807754141@qq.com>
2024-07-04 14:52:33 +0800

    change sleep time to 2

--- login/auto_login.py
+++ login/auto_login.py
@@ -7,7 +7,7 @@ time.sleep(2)
 username = "pansonglin" # 请替换成你的用户名
 password = "tenda0043408..." # 请替换成你的密码
 driver.find_element(By.ID,'password_name').send_keys(username) # 自动敲入用户名
-driver.find_element(By.ID,'password_pwd').send_keys(password) # 自动敲入密码
+driver.find_element(By.ID,'password_pwd').send_keys(password) 
 a=driver.find_element(By.NAME,'btlogin')
 a.click()
 time.sleep(2)
